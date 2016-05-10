c9c1302c6e798652ca7be204735237d53eb80419
qaqaqa <1149140003@qq.com>
2016-05-10 17:19:07 +0800

    add camera

---
+++ ReduxTest/index.ios.js
@@ -0,0 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {Root} from './app/root';
+import {AppRegistry} from 'react-native';
+AppRegistry.registerComponent('ReduxTest', () => Root);
--- index.android.js
+++
@@ -1,8 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-"use strict";
-const react_native_1 = require('react-native');
-const navigator_android_1 = require('./android-js/navigator.android');
-react_native_1.AppRegistry.registerComponent('ReactNativeGlood', () => navigator_android_1.NavigatorClass);
