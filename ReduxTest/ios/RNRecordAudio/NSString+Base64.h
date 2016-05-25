2161c3744c1ce2e01ea8a2fd6864ee483d868f80
qaqaqa <1149140003@qq.com>
2016-05-25 17:08:44 +0800

    add base64

---
+++ ReduxTest/ios/RNRecordAudio/NSString+Base64.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Base64.h
+//  ReduxTest
+//
+//  Created by sparxo-dev-ios-1 on 16/5/20.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString_Base64 : NSObject
+
+@end
---
+++ ReduxTest/ios/RNRecordAudio/NSString+Base64.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Base64.m
+//  ReduxTest
+//
+//  Created by sparxo-dev-ios-1 on 16/5/20.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import "NSString+Base64.h"
+
+@implementation NSString_Base64
+
+@end
