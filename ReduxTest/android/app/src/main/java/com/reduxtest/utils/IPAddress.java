cdd0b6fe01a7f4e5674f23ef41023865b2151f10
Bad leadership <1754533217@qq.com>
2016-06-13 16:10:48 +0800

    支持获取蜂窝IP

--- ReduxTest/android/app/src/main/java/com/reduxtest/utils/IPAddress.java
+++ ReduxTest/android/app/src/main/java/com/reduxtest/utils/IPAddress.java
@@ -1,18 +1,32 @@
 package com.reduxtest.utils;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Enumeration;
 
 /**
  * Created by Sparxo_Android on 2016/6/8.
  */
 public class IPAddress {
+    /**
+     * 获取wifi地址
+     *
+     * @param ipaddr
+     * @return
+     */
     public static String GetNetIp(String ipaddr) {
         URL infoUrl = null;
         InputStream inStream = null;
@@ -32,12 +46,55 @@ public class IPAddress {
                 return strber.toString();
             }
         } catch (MalformedURLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return "";
+        return getPhoneIp();
+    }
+
+    /**
+     * 移动数据上网IP
+     *
+     * @return
+     */
+    public static String getPhoneIp() {
+        try {
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                NetworkInterface intf = en.nextElement();
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                    InetAddress inetAddress = enumIpAddr.nextElement();
+                    if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                        return inetAddress.getHostAddress().toString();
+                    }
+                }
+            }
+        } catch (Exception e) {
+        }
+        return "127.0.0.1";
+    }
+
+    public static String getAndroidIpAddress(Context context) {
+        String result = null;
+        try {
+            WifiManager wifi = (WifiManager) context
+                    .getSystemService(Context.WIFI_SERVICE);
+            WifiInfo info = wifi.getConnectionInfo();
+            result = intToIp(info.getIpAddress());
+        } catch (Exception e) {
+            result = getPhoneIp();
+        }
+        if (result == null)
+            result = getPhoneIp();
+        if (result.equals("0.0.0.0"))
+            result = getPhoneIp();
+        return result;
+    }
+
+    private static String intToIp(int i) {
+        return (i & 0xFF) + "." +
+                ((i >> 8) & 0xFF) + "." +
+                ((i >> 16) & 0xFF) + "." +
+                (i >> 24 & 0xFF);
     }
 }
