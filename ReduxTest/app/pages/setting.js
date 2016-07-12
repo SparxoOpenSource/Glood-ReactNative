2dbe33f3f2cf999efce6dd566e2a065fc6e334fd
qaqaqa <1149140003@qq.com>
2016-07-12 11:04:25 +0800

    修改setting UI

--- ReduxTest/app/pages/setting.js
+++ ReduxTest/app/pages/setting.js
@@ -14,7 +14,9 @@ import { AppRegistry,
     Animated,
     Dimensions,
     UIManager,
-    Easing }  from 'react-native';
+    Easing,
+    Switch,
+}  from 'react-native';
 import {Common} from "./common";
 import isAndroid from '../utils/isAndroid.js';
 var widthh = Dimensions.get('window').width
@@ -30,25 +32,33 @@ export class Setting extends Component {
         super();
         this.state = {
         }
+        this._toggleTransparent = this.toggleTransparent.bind(this);
     }
+    toggleTransparent() {
+    this.setState({transparent: !this.state.transparent});
+  }
     render() {
         return (
             <Image style={style.container} source={require('../img/background3.png') }>
                 <Common navigator={this.props.navigator} title={this.props.title}/>
-                <View style={{ width: widthh, height: heightt, backgroundColor: '#00000000', marginTop: heightt*(22/736) }}>
+                <View style={{ width: widthh, height: heightt, backgroundColor: '#00000000', marginTop: heightt * (22 / 736) }}>
                     <Text style={{
                         color: 'black', fontSize: heightt * (27 / 736), width: widthh * (300 / 414), fontFamily: 'OpenSans',
-                        marginLeft: widthh*(30/414)
+                        marginLeft: widthh * (30 / 414)
                     }}>Conversation Notification</Text>
+                    <Switch style={{marginTop:heightt*(-38/736),marginLeft:widthh*(320/414),height:heightt*(28/736),width:widthh*(52/414)}}
+                    onValueChange={this._toggleTransparent}/>
                     <Text style={{
                         color: 'black', fontSize: heightt * (27 / 736), width: widthh * (300 / 414), fontFamily: 'OpenSans',
-                        marginLeft: widthh*(30/414), marginTop:heightt*(60/736)
+                        marginLeft: widthh * (30 / 414), marginTop: heightt * (60 / 736)
                     }}>Event Organizer</Text>
-                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#53aeee', width: widthh, height: heightt * (70 / 736), marginTop: heightt * (47 / 736) }}
+                    <Switch style={{marginTop:heightt*(-38/736),marginLeft:widthh*(320/414),height:heightt*(28/736),width:widthh*(52/414)}}></Switch>
+                    <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#53aeee', width: widthh, height: heightt * (70 / 736), 
+                    marginTop: heightt * ((736-70-27-27-60-5) / 736) }}
                         onPress={this._logout.bind(this) }>
                         <Text style={{
                             backgroundColor: '#00000000',
-                            color: 'white', fontSize: heightt * (32 / 736), width: widthh * (110 / 414), fontFamily: 'OpenSans-Bold', marginTop: heightt * ((70-50) / 736) / 2,
+                            color: 'white', fontSize: heightt * (32 / 736), width: widthh * (110 / 414), fontFamily: 'OpenSans-Bold', marginTop: heightt * ((70 - 50) / 736) / 2,
                             marginLeft: (widthh - (widthh * (80 / 414))) / 2
                         }}>logout</Text>
                     </TouchableOpacity>
@@ -56,7 +66,7 @@ export class Setting extends Component {
             </Image>
         );
     }
-    _logout(){
+    _logout() {
 
     }
 }
