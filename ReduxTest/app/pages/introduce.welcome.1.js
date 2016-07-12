075d478a2ed6aa5ac09e8695bc74ecc506ff01e3
Bad leadership <1754533217@qq.com>
2016-07-12 11:59:13 +0800

    new introduce

--- ReduxTest/app/pages/introduce.welcome.1.js
+++ ReduxTest/app/pages/introduce.welcome.1.js
@@ -27,25 +27,51 @@ const propTypes = {
 /**
  * 介绍
  */
-export class IntroduceWelcome extends Component {
+export class IntroduceWelcomeButton extends Component {
     render() {
         return (
             <Image style={styles.container} source={require('../img/background3.png') }>
-                <View>
+                <View style={{ flex: 5 }}>
                     <Text style={styles.slide}>{this.props.content}</Text>
                 </View>
+                <TouchableOpacity style={{
+                    flexDirection: 'row', flex: 1,
+                    marginTop: 16
+                }}
+                    onPress={this.go.bind(this) }>
+                    <Text style={{
+                        backgroundColor: 'transparent',
+                        fontSize: fontSizeAndroid(40),
+                        color: "#FFFFFF",
+                        fontFamily: "OpenSans-Light",
+                        fontWeight: "normal",
+                        marginLeft: 36,
+                        alignItems: 'center',
+                    }}>get started</Text>
+                    <Image source={require("../img/go.jpg") }
+                        style={{
+                            alignItems: 'center', width: 60, height: 35, marginLeft: 30,
+                            marginTop: 6
+                        }}/>
+                </TouchableOpacity>
             </Image>
         );
     }
+    go() {
+        this.props.navigator.replace({
+            name: "Login", value: "Login", nav: this.props.navigator
+        });
+    }
 }
 
-IntroduceWelcome.propTypes = propTypes;
+IntroduceWelcomeButton.propTypes = propTypes;
 
 var styles = StyleSheet.create({
     container: {
         height: height,
         width: width,
-        flexDirection: "column"
+        flexDirection: "column",
+        flex: 6
     },
     slide: {
         flex: 1,
