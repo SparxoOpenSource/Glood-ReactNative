4056325ed74ac894a297599bc30e2d7c14861045
qaqaqa <1149140003@qq.com>
2016-07-12 12:20:39 +0800

    add setting ui switch

--- ReduxTest/app/pages/setting.js
+++ ReduxTest/app/pages/setting.js
@@ -14,11 +14,11 @@ import { AppRegistry,
     Animated,
     Dimensions,
     UIManager,
-    Easing,
-    Switch,
+    Easing
 }  from 'react-native';
 import {Common} from "./common";
 import isAndroid from '../utils/isAndroid.js';
+var Switch = require('react-native-material-switch');
 var widthh = Dimensions.get('window').width
 var STATUS_BAR_HEIGHT = Navigator.NavigationBar.Styles.General.StatusBarHeight;
 var heightt = Dimensions.get('window').height - Navigator.NavigationBar.Styles.General.NavBarHeight - 20
@@ -32,29 +32,40 @@ export class Setting extends Component {
         super();
         this.state = {
         }
-        this._toggleTransparent = this.toggleTransparent.bind(this);
     }
-    toggleTransparent() {
-    this.setState({transparent: !this.state.transparent});
-  }
     render() {
         return (
             <Image style={style.container} source={require('../img/background3.png') }>
                 <Common navigator={this.props.navigator} title={this.props.title}/>
-                <View style={{ width: widthh, height: heightt, backgroundColor: '#00000000', marginTop: heightt * (22 / 736) }}>
+                <View style={{ width: widthh, height: heightt, backgroundColor: '#00000000', marginTop: heightt * (35 / 736) }}>
                     <Text style={{
                         color: 'black', fontSize: heightt * (27 / 736), width: widthh * (300 / 414), fontFamily: 'OpenSans',
                         marginLeft: widthh * (30 / 414)
                     }}>Conversation Notification</Text>
-                    <Switch style={{marginTop:heightt*(-38/736),marginLeft:widthh*(320/414),height:heightt*(28/736),width:widthh*(52/414)}}
-                    onValueChange={this._toggleTransparent}/>
+                    <View style={{marginTop:heightt*(-32/736),marginLeft:widthh*(330/414)}}>
+                        <Switch style={{backgroundColor:"#00000000"}}
+                        buttonRadius={14} switchHeight={17} switchWidth={45} 
+                        activeBackgroundColor='rgba(12, 212, 196, 0.50)'
+                        inactiveBackgroundColor='rgba(172, 172, 172, 0.70)'
+                        activeButtonColor='rgba(12, 212, 196, 1)'
+                        inactiveButtonPressedColor='rgba(254, 255, 255, 1)'
+                        onChangeState={(state) => alert('change state '+ state)}/>
+                    </View>
                     <Text style={{
                         color: 'black', fontSize: heightt * (27 / 736), width: widthh * (300 / 414), fontFamily: 'OpenSans',
                         marginLeft: widthh * (30 / 414), marginTop: heightt * (60 / 736)
                     }}>Event Organizer</Text>
-                    <Switch style={{marginTop:heightt*(-38/736),marginLeft:widthh*(320/414),height:heightt*(28/736),width:widthh*(52/414)}}></Switch>
+                    <View style={{marginTop:heightt*(-32/736),marginLeft:widthh*(330/414)}}>
+                        <Switch style={{backgroundColor:"#00000000"}}
+                        buttonRadius={14} switchHeight={17} switchWidth={45} 
+                        activeBackgroundColor='rgba(12, 212, 196, 0.50)'
+                        inactiveBackgroundColor='rgba(172, 172, 172, 0.70)'
+                        activeButtonColor='rgba(12, 212, 196, 1)'
+                        inactiveButtonPressedColor='rgba(254, 255, 255, 1)'
+                        onChangeState={(state) => alert('change state '+ state)}/>
+                    </View>
                     <TouchableOpacity style={{ flexDirection: 'row', backgroundColor: '#53aeee', width: widthh, height: heightt * (70 / 736), 
-                    marginTop: heightt * ((736-70-27-27-60-5) / 736) }}
+                    marginTop: heightt * ((736-70-27-27-60-20-17) / 736) }}
                         onPress={this._logout.bind(this) }>
                         <Text style={{
                             backgroundColor: '#00000000',
