71d9a4a45a4470ea6713d8eec46671538132a048
Bad leadership <1754533217@qq.com>
2016-08-26 15:22:39 +0800

    a little

--- ReduxTest/app/pages/db.js
+++ ReduxTest/app/pages/db.js
@@ -2,13 +2,13 @@ import React, {Component} from "react";
 import { AppRegistry, StyleSheet, View, Text, ListView, Alert, Navigator, Image, TouchableOpacity, BackAndroid,
     Platform, Dimensions, PropTypes, DeviceEventEmitter, NativeModules}  from 'react-native';
 import Button from '@remobile/react-native-simple-button';
-import {Add, Select, DeleteMin, Drop, Update} from "../utils/DBUtil"
+import {Add, Select, DeleteMin, Drop, Update, DoTest} from "../utils/DBUtil"
 
 export class DBPage extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Button onPress={this.doTest.bind(this) }>测试</Button>
+                <Button onPress={DoTest.bind(this) }>测试</Button>
                 <Button onPress={Add.bind(this) }>add</Button>
                 <Button onPress={DeleteMin.bind(this) }>deleteMin</Button>
                 <Button onPress={Update.bind(this) }>update</Button>
